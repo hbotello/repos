5bf10bcb4a187dd51eecd2466aa345096eb46fb1
heriberto botello <hbotellonasta@hotmail.com>
2019-02-01 16:19:18 -0600

    Repositorio de Empleados Terminado

--- Inventario.DAL/RepositorioDeEmpleados.cs
+++ Inventario.DAL/RepositorioDeEmpleados.cs
@@ -1,29 +1,84 @@
 ﻿using Inventario.COMMON.Entidades;
 using Inventario.COMMON.Interfaces;
+using LiteDB;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Inventario.DAL
 {
     public class RepositorioDeEmpleados : IRepositorio<Empleado>
     {
-        //Base de datos NoSQL->Orientada a Documentos->LiteDB
-        public List<Empleado> Read => throw new NotImplementedException();
+        private readonly string DBName = "Inventario.db";
+        private readonly string TableName = "Empleados";
+
+
+        public List<Empleado> Read {
+            get
+            {
+                List<Empleado> datos = new List<Empleado>();
+                using (var db = new LiteDatabase(DBName))
+                {
+                    datos = db.GetCollection<Empleado>(TableName).FindAll().ToList();
+                }
+                return datos;
+            }
+
+        }
 
         public bool Create(Empleado entidad)
         {
-            throw new NotImplementedException();
+            entidad.Id = Guid.NewGuid().ToString();
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Empleado>(TableName);
+                    coleccion.Insert(entidad);
+                }
+                return true;
+            }
+            catch(Exception)
+            {
+               return false;
+            }
         }
 
-        public bool Delete(Empleado entidad)
+        public bool Delete(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int r;
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Empleado>(TableName);
+                    r = coleccion.Delete(e => e.Id == id);
+                    
+                }
+               
+                return r > 0;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
         }
 
-        public bool Update(Empleado entidadOriginal, Empleado entidadModificada)
+        public bool Update(Empleado entidadModificada)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Empleado>(TableName);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
